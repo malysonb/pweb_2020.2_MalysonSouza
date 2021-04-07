a3080c3907e041e94adf88a9331291ce95fdbf89
Malyson Breno <7494537+malysonb@users.noreply.github.com>
2021-04-07 01:58:43 -0300

    R do crud feito! cheio de bug ainda

--- cadpessoas_malysonsouza/src/main/java/br/com/malysonsouza/cadpessoas/controllers/PessoaController.java
+++ cadpessoas_malysonsouza/src/main/java/br/com/malysonsouza/cadpessoas/controllers/PessoaController.java
@@ -1,10 +1,35 @@
 package br.com.malysonsouza.cadpessoas.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.malysonsouza.cadpessoas.model.Pessoa;
+import br.com.malysonsouza.cadpessoas.repositories.PessoaRepository;
 
 @Controller
 @RequestMapping("/")
 public class PessoaController {
 
+	@Autowired
+	PessoaRepository pessoaRepo;
+	
+	@GetMapping
+	public String index() {
+		return "index.html";
+	}
+	
+	@GetMapping("/listarPessoas")
+	public ModelAndView listarPessoas() {
+		List<Pessoa> lista = pessoaRepo.findAll();
+		ModelAndView mav = new ModelAndView("listarPessoas");
+		mav.addObject("pessoas", lista);
+		return mav;
+	}
+	
 }
