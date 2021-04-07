089b8e781c0d2ccd76e70be078ef816a9f5ce4bb
Malyson Breno <7494537+malysonb@users.noreply.github.com>
2021-04-07 16:16:24 -0300

    CRUD completo

--- cadpessoas_malysonsouza/src/main/java/br/com/malysonsouza/cadpessoas/controllers/PessoaController.java
+++ cadpessoas_malysonsouza/src/main/java/br/com/malysonsouza/cadpessoas/controllers/PessoaController.java
@@ -2,9 +2,11 @@ package br.com.malysonsouza.cadpessoas.controllers;
 
 import java.util.List;
 
+import org.dom4j.IllegalAddException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,6 +32,41 @@ public class PessoaController {
 		ModelAndView mav = new ModelAndView("listarPessoas");
 		mav.addObject("pessoas", lista);
 		return mav;
+	}
+	
+	@GetMapping("/adicionarPessoa")
+	public ModelAndView formAdicionarPessoa() {
+		ModelAndView mav = new ModelAndView("adicionarPessoa");
+		mav.addObject(new Pessoa());
+		return mav;
+	}
+	
+	@PostMapping("/adicionarPessoa")
+	public String adicionarPessoa(Pessoa p) {
+		this.pessoaRepo.save(p);
+		return "redirect:/listarPessoas";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public ModelAndView formEditarPessoa(@PathVariable("id") long id) {
+		Pessoa pessoa = pessoaRepo.findById(id).orElseThrow(()-> new IllegalAddException("Este id é invalido: " + id));
+		ModelAndView mav = new ModelAndView("editarPessoa");
+		mav.addObject(pessoa);
+		return mav;
+	}
+	
+	@PostMapping("/editar/{id}")
+	public ModelAndView editarPessoa(@PathVariable("id") long id, Pessoa pessoa)
+	{
+		this.pessoaRepo.save(pessoa);
+		return new ModelAndView("redirect:/listarPessoas");
+	}
+	
+	@GetMapping("/remover/{id}")
+	public ModelAndView removerPessoa(@PathVariable("id") long id) {
+		Pessoa aRemover = pessoaRepo.findById(id).orElseThrow(()-> new IllegalArgumentException("Este id é invalido: " + id));
+		pessoaRepo.delete(aRemover);
+		return new ModelAndView("redirect:/listarPessoas");
 	}
 	
 }
