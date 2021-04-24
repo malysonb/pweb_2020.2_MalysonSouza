71f691d70b3e9e3f99bd036eff936f83b06b7033
Malyson Breno <7494537+malysonb@users.noreply.github.com>
2021-04-24 19:02:15 -0300

    Vamos começar a unidade 2
    meio tarde, mas vamos la

---
+++ agropopshop_Malysonsouza/src/main/java/br/com/malysonsouza/agropopshop/AgropopshopMalysonsouzaApplication.java
@@ -0,0 +1,13 @@
+package br.com.malysonsouza.agropopshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgropopshopMalysonsouzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgropopshopMalysonsouzaApplication.class, args);
+	}
+
+}
---
+++ agropopshop_Malysonsouza/src/test/java/br/com/malysonsouza/agropopshop/AgropopshopMalysonsouzaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.malysonsouza.agropopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgropopshopMalysonsouzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
