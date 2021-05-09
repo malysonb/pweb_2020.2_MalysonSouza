1147bfd8d4263761adf53fe95fb678610d552235
Malyson Breno <7494537+malysonb@users.noreply.github.com>
2021-05-09 18:53:31 -0300

    Iniciando US3 e US4

---
+++ agropopshop_malysonsouza/src/main/java/br/com/malysonsouza/agropopshop/model/Pedido.java
@@ -0,0 +1,35 @@
+package br.com.malysonsouza.agropopshop.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "pedidos")
+public class Pedido implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private LocalDate dataVenda;
+    private String formaPagamento;
+    private int qnt;
+
+    @OneToMany(mappedBy = "pedidoProduto")
+    private List<Produto> pedidoProduto;
+}
